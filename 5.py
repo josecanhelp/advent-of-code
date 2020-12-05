40a96c59fe593f61af6e1f9ce284755f60038445
Jose Soto <josecanhelp@gmail.com>
2020-12-05 11:02:10 -0800

    Day 5 improvements

--- 5.py
+++ 5.py
@@ -1,17 +1,18 @@
-# %%
-input = open("input-day5.txt", "r")
-# %%
+input = list(open("input-day5.txt", "r"))
+
+
+def splitTicket(line):
+    return {
+        'row': line[0:7].replace("F", "0").replace("B", "1"),
+        'col': line[-4::].replace("\n", "").replace("R", "1").replace("L", "0")
+    }
 
 
 def getSeatId(line):
-    rowBin = line[0:7].replace("F", "0").replace("B", "1")
-    colBin = line[-3::].replace("R", "1").replace("L", "0")
-    row = int(rowBin, 2)
-    col = int(colBin, 2)
-    seatId = int(rowBin, 2) * 8 + int(colBin, 2)
-    return seatId
+    return int(splitTicket(line)['row'], 2) * 8 + int(splitTicket(line)['col'], 2)
 
 
-# %%
-print(max(getSeatId(x) for x in input))  # 888
-# %%
+maxSeatId = max(getSeatId(x) for x in input)  # 888
+minSeatId = min(getSeatId(x) for x in input)  # 89
+mySeatId = list(set(list((getSeatId(x) for x in input))) ^ set(
+    list(range(minSeatId, maxSeatId + 1))))[0]  # 522
