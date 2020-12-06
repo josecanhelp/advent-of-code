010b79003643d32aab8bc377c93962e7415400e5
Jose Soto <josecanhelp@gmail.com>
2020-12-06 08:38:11 -0800

    Improvements Day 6

--- 6.py
+++ 6.py
@@ -2,19 +2,20 @@
 import math
 import functools
 
-with open("input-day6.txt", "r") as f:
-    input = [line.split() for line in f.read().strip().split("\n\n")]
+input = [line.split() for line in open(
+    'input-day6.txt', "r").read().split("\n\n")]
 
 
 def getUnique(i):
     return set(i)
 
 
-def findSimilarities(i, j):
+def getSimilars(i, j):
     return set(i) & set(j)
 
 
 print(math.fsum((len(getUnique(x))
-                 for x in (''.join(x) for x in input))))  # 3382
-print(math.fsum([len(functools.reduce(findSimilarities, x)) for x in input]))
+                 for x in (''.join(y) for y in input))))  # 6662
+
+print(math.fsum((len(functools.reduce(getSimilars, x)) for x in input)))  # 3382
 # %%
