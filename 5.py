40ce5990267fe7c4988dac0256797e6b2014c890
Jose Soto <josecanhelp@gmail.com>
2020-12-06 07:06:50 -0800

    minor cleanup day 5

--- 5.py
+++ 5.py
@@ -1,11 +1,16 @@
+# %%
 input = list(open("input-day5.txt", "r"))
+
+# Convert ticket string into dict of binary strings
 
 
 def splitTicket(line):
     return {
         'row': line[0:7].replace("F", "0").replace("B", "1"),
-        'col': line[-4::].replace("\n", "").replace("R", "1").replace("L", "0")
+        'col': line[-4::].rstrip("\n").replace("L", "0").replace("R", "0")
     }
+
+# Convert binary strings to int and generate Seat ID
 
 
 def getSeatId(line):
@@ -16,3 +21,4 @@ maxSeatId = max(getSeatId(x) for x in input)  # 888
 minSeatId = min(getSeatId(x) for x in input)  # 89
 mySeatId = list(set(list((getSeatId(x) for x in input))) ^ set(
     list(range(minSeatId, maxSeatId + 1))))[0]  # 522
+# %%
